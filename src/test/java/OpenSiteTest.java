10eddc290cf8a73435b978e39e8ddab389111a68
Bondarev Serj <b-sergi@mail.ru>
2020-04-23 00:25:56 +0500

    added browser selection support using maven params

--- src/test/java/OpenSiteTest.java
+++ src/test/java/OpenSiteTest.java
@@ -1,39 +1,55 @@
 import Pages.MainPage;
 import config.ServerConfig;
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.aeonbits.owner.ConfigFactory;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Listeners;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
+
 
 @Listeners(ExecutionListener.class)
 public class OpenSiteTest {
 
-    protected static WebDriver driver;
+    protected WebDriver driver;
     private static final Logger logger = LogManager.getLogger(OpenSiteTest.class);
     private ServerConfig cfg = ConfigFactory.create(ServerConfig.class);
+    private String browserName;
+
+    @BeforeSuite
+    public void browserInit(){
+        logger.info("Инициализация Suite");
+        browserName = System.getProperty("browser").replace("'", "");   //забираем параметр введенный через maven
+        browserName = browserName.toUpperCase();
+        logger.info(browserName);
+
+        if(browserName.isEmpty())                                                   //браузер устанавливается в порядке приоритета:
+            if(!cfg.browser().isEmpty())                                            //1. параметра в консоли
+                browserName = cfg.browser();                                        //2. параметра в Pom.xml
+            else{                                                                   //3. config.properties
+                logger.error("Имя браузера не задано");                          //и если нигде мы не нашли название браузера, кидаем исключение
+                throw new NullPointerException("Имя браузера не задано");
+            }
+    }
 
     @BeforeTest
-    public void setUp(){
+    public void setUp() throws NullPointerException {
         logger.info("Инициализация теста");
 
-        switch (cfg.browser().toLowerCase()){
-            case ("firefox"):                                           //заготовка для расширения пула браузеров
-                WebDriverManager.firefoxdriver().setup();
-                driver = new FirefoxDriver();
-            default:                                                    //если иного не задано, то запускаем хром
-                WebDriverManager.chromedriver().setup();
-                driver = new ChromeDriver();
+        switch (browserName){
+            case ("FIREFOX"):
+             //   FirefoxOptions firefoxOptions = new FirefoxOptions();
+             //   firefoxOptions.addArguments("--incognito");                              //здесь будут какие либо опции
+                driver = WebDriverFactory.create(browserName);
+                break;
+            default:
+                ChromeOptions chromeOptions = new ChromeOptions();                    //для включения ChromeOptions раскоменть это
+                chromeOptions.addArguments("disable-extensions");
+                driver = WebDriverFactory.create(browserName, chromeOptions);
+                break;
         }
         driver.manage().window().maximize();
-        logger.info("Запущен драйвер для " + cfg.browser());
     }
 
     @Test(description = "Checking logo on main page")
